9dc3f407217c3aed9b4f66643f79e3e787d334b7
satyam kumar <satyamsingh83840@gmail.com>
2026-07-15 23:04:54 +0530

    Update contact-form.jsx

--- components/contact/contact-form.jsx
+++ components/contact/contact-form.jsx
@@ -37,7 +37,7 @@ export default function ContactForm() {
     // Construct the payload for Web3Forms
     const formData = {
       // TODO: Replace with your actual Web3Forms Access Key
-      access_key: "629f2234-cbf0-4997-b565-38b0dc0f843e",
+      access_key: "573875a5-6b5b-4844-ba0a-969633787525",
       subject: form.subject || `New Contact Form Submission from ${form.name}`,
       ...form,
     };
